0f20532422acc52c719dabb7073ee1d1e344740f
J. Cohen <hostelmarketing@gmail.com>
2020-01-22 14:07:22 -0800

    Add leetcode files

--- scrapers/bots/leetcode.py
+++ scrapers/bots/leetcode.py
@@ -1,1 +1,10 @@
 # TODO
+
+"""
+The query selectors for extracting title and difficulty from leetcode problems.
+
+```javascript
+const title = document.querySelector(`[data-cy="question-title"]`).innerText.trim();
+const difficulty = document.querySelector("div[diff]").innerText.trim();
+```
+"""
