bcb2cb2ed2373475175f74988ee55dd23b24674e
J. Cohen <hostelmarketing@gmail.com>
2020-01-22 05:32:40 -0800

    Restructure

--- express_api/routes/puzzles.js
+++ express_api/routes/puzzles.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-    res.json({ title: "home" });
+    res.json({ title: "puzzles" });
 });
 
 module.exports = router;
