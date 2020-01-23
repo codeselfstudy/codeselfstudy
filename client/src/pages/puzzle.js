186fddcf80411625d40f779dbf5b58c8eb7b1af9
J. Cohen <hostelmarketing@gmail.com>
2020-01-22 20:00:44 -0800

    Add request for backend data from client

--- client/src/pages/puzzle.js
+++ client/src/pages/puzzle.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
-export default function Puzzle(props) {
+export default function Puzzle() {
     const { id } = useParams();
+    const [puzzleData, setPuzzleData] = useState("loading");
+
+    useEffect(() => {
+        axios.get(`/api/puzzles/${id}`).then(res => setPuzzleData(res));
+    }, [id]);
+
     return (
         <div>
             <h1>Coding Puzzle #{id}</h1>
             <p>puzzle description goes here</p>
+            <code>{JSON.stringify(puzzleData)}</code>
         </div>
     );
 }
