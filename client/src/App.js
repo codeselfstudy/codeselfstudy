f8837002aa8ab3a84ad52c6d0cd135000aa34137
J. Cohen <hostelmarketing@gmail.com>
2020-01-22 21:44:42 -0800

    Remove tailwinds

--- client/src/App.js
+++ client/src/App.js
@@ -10,30 +10,15 @@ function App() {
     return (
         <Router>
             <nav>
-                <ul className="flex border-b">
-                    <li className="-mb-px mr-1">
-                        <Link
-                            className="bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold"
-                            to="/"
-                        >
-                            Home
-                        </Link>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
                     </li>
-                    <li className="mr-1">
-                        <Link
-                            className="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold"
-                            to="/about"
-                        >
-                            About
-                        </Link>
+                    <li>
+                        <Link to="/about">About</Link>
                     </li>
-                    <li className="mr-1">
-                        <Link
-                            className="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold"
-                            to="/puzzles"
-                        >
-                            Puzzles
-                        </Link>
+                    <li>
+                        <Link to="/puzzles">Puzzles</Link>
                     </li>
                 </ul>
             </nav>
