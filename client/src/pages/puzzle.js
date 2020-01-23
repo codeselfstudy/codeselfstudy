b8e9321277c6c9b9d55b744baac827c4c8527d81
J. Cohen <hostelmarketing@gmail.com>
2020-01-22 22:42:15 -0800

    Add spinner

--- client/src/pages/puzzle.js
+++ client/src/pages/puzzle.js
@@ -1,13 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import Spinner from "../components/Spinner";
 
 export default function Puzzle() {
     const { id } = useParams();
-    const [puzzleData, setPuzzleData] = useState("loading");
+    const [puzzleData, setPuzzleData] = useState();
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        axios.get(`/api/puzzles/${id}`).then(res => setPuzzleData(res));
+        axios.get(`/api/puzzles/${id}`).then(res => {
+            setTimeout(() => {
+                setPuzzleData(res);
+                setIsLoading(false);
+            }, 500);
+        });
     }, [id]);
 
     return (
@@ -15,6 +22,7 @@ export default function Puzzle() {
             <h1 className="text-4xl">Coding Puzzle #{id}</h1>
             <p>puzzle description goes here</p>
             <code>
+                {isLoading ? <Spinner /> : <></>}
                 <pre>{JSON.stringify(puzzleData, null, 4)}</pre>
             </code>
         </div>
