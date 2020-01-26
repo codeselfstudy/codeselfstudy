bdd976fbee33fd34716b27c11b20462e84b6a678
J. Cohen <hostelmarketing@gmail.com>
2020-01-26 03:09:42 -0800

    Clean up the placeholder data

--- containers/gatsby/src/pages/index.js
+++ containers/gatsby/src/pages/index.js
@@ -6,6 +6,36 @@ import Layout from "../components/Layout";
 import SEO from "../components/SEO";
 import Spinner from "../components/Spinner";
 
+function DemoTable({ data }) {
+    const entries = Object.entries(data);
+
+    return (
+        <table
+            style={{ border: "2px dotted #666" }}
+            className="table is-responsive"
+        >
+            <thead>
+                <tr>
+                    <th>key</th>
+                    <th>value</th>
+                </tr>
+            </thead>
+            <tbody>
+                {entries.map(i => (
+                    <tr>
+                        <td>
+                            <strong>{i[0]}</strong>
+                        </td>
+                        <td>
+                            <code>{JSON.stringify(i[1])}</code>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+}
+
 export default function IndexPage() {
     const [isLoading, setIsLoading] = useState(true);
     const [apiData, setApiData] = useState(null);
@@ -28,11 +58,9 @@ export default function IndexPage() {
             <section className="section">
                 <h1 className="title is-1">Hello World</h1>
                 <p>This should load some fake data from the Express API.</p>
-                {isLoading ? (
-                    <Spinner />
-                ) : (
-                    <code>{JSON.stringify(apiData)}</code>
-                )}
+
+                {isLoading ? <Spinner /> : <DemoTable data={apiData} />}
+
                 <p>
                     There's also a <Link to="/page-2/">page 2</Link>.
                 </p>
