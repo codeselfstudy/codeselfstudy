a0698a403541f6fc0f38e192da4504567a3ab39e
J. Cohen <hostelmarketing@gmail.com>
2020-01-28 19:48:31 -0800

    Working scraper

--- scrapers/lib/scrapers/project_euler.ex
+++ scrapers/lib/scrapers/project_euler.ex
@@ -5,27 +5,32 @@ defmodule Scrapers.ProjectEuler do
   use HTTPoison.Base
 
   @base_url "https://projecteuler.net/problem="
-  # @number_of_problems 624
-  @number_of_problems 3
+  @output_dir File.cwd!() <> "/../seed_data/project_euler"
+  @start_id 1
+  @end_id 624
   # Milliseconds to wait between fetches
-  @delay 1000 * 2
+  @delay 1000 * 1
 
   def fetch_puzzle(id) do
     url = create_url(id)
+    IO.puts("fetching #{url}")
+
+    :timer.sleep(@delay)
 
     case HTTPoison.get(url) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} -> body
       {:ok, %HTTPoison.Response{status_code: 404}} -> "Not found :("
       {:error, %HTTPoison.Error{reason: reason}} -> reason
+      x -> IO.inspect(x)
     end
   end
 
   def process_response_body(body) do
     {:ok, document} = Floki.parse_document(body)
 
-    [{"h2", _, [problem_name]} | _] = Floki.find(document, "#content h2")
+    [{"h2", _, [problem_name | _]} | _] = Floki.find(document, "#content h2")
     [{"div", _, problem_content} | _] = Floki.find(document, "div.problem_content")
-    [{"h3", _, [problem_info]} | _] = Floki.find(document, "div#problem_info h3")
+    [{"h3", _, [problem_info | _]} | _] = Floki.find(document, "div#problem_info h3")
     regex = ~r"Problem (\d+)"
     [_, id] = Regex.run(regex, problem_info)
 
@@ -38,14 +43,16 @@ defmodule Scrapers.ProjectEuler do
   end
 
   # TODO: write this function
-  def save_json_file(j) do
-    IO.inspect(j)
-    :timer.sleep(@delay)
-    j
+  def save_json_file(%{:id => id} = m) do
+    IO.inspect(m)
+    filename = @output_dir <> "/" <> id <> ".json"
+    content = Jason.encode!(m)
+    File.write(filename, content)
+    IO.puts("wrote #{filename}")
   end
 
   def get_all_problems() do
-    Enum.map(1..@number_of_problems, &fetch_puzzle/1)
+    Enum.map(@start_id..@end_id, &fetch_puzzle/1)
     |> Enum.map(&process_response_body/1)
     |> Enum.map(&save_json_file/1)
   end
