29a1bba0d0d2908bd3c20b4c63e4e071bc5472da
J. Cohen <hostelmarketing@gmail.com>
2020-02-14 13:11:13 -0800

    Add first attempt at PageTemplate.js

---
+++ containers/gatsby/src/templates/PageTemplate.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { graphql, Link } from "gatsby";
+
+import Layout from "../components/Layout";
+
+/**
+ * This is the template for the website's basic content pages.
+ */
+export default function PageTemplate({
+    data, // this prop will be injected by the GraphQL query below.
+}) {
+    const { markdownRemark } = data; // data.markdownRemark holds your post data
+    const { frontmatter, html } = markdownRemark;
+
+    return (
+        <Layout>
+            <section className="section">
+                <div className="container content">
+                    <h1 className="title is-1">{frontmatter.title}</h1>
+                    <div
+                        className="page-content"
+                        dangerouslySetInnerHTML={{ __html: html }}
+                    />
+                </div>
+            </section>
+        </Layout>
+    );
+}
+
+export const pageQuery = graphql`
+    query($path: String!) {
+        markdownRemark(frontmatter: { path: { eq: $path } }) {
+            html
+            frontmatter {
+                date(formatString: "MMMM DD, YYYY")
+                path
+                title
+            }
+        }
+    }
+`;
