a90f31e54481eeb28b3520a6ed229c23807ff4b9
J. Cohen <hostelmarketing@gmail.com>
2020-02-14 20:25:55 -0800

    It builds again

--- containers/gatsby/gatsby-node.js
+++ containers/gatsby/gatsby-node.js
@@ -5,10 +5,26 @@
  */
 
 const path = require("path");
+const { get, each } = require("lodash");
+
+// exports.onCreateNode = ({ node, actions, getNode }) => {
+//     const { createNodeField } = actions;
+
+//     if (get(node, "internal.type") === "MarkdownRemark") {
+//         const parent = getNode(get(node, "parent"));
+//         createNodeField({
+//             node,
+//             name: "collection",
+//             value: get(parent, "sourceInstanceName"),
+//         });
+//     }
+// };
 
 exports.createPages = async ({ actions, graphql, reporter }) => {
     const { createPage } = actions;
-    const blogPostTemplate = path.resolve("src/templates/PostTemplate.js");
+    const postTemplate = path.resolve("src/templates/PostTemplate.js");
+    const pageTemplate = path.resolve("src/templates/pageTemplate.js");
+
     const result = await graphql(`
         {
             allMarkdownRemark(
@@ -30,10 +46,20 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
         reporter.panicOnBuild(`Error while running GraphQL query.`);
         return;
     }
+
+    // Separate the collection types
+    const allEdges = result.data.allMarkdownRemark.edges;
+    // const postEdges = allEdges.filter(
+    //     edge => edge.node.fields.collection === "posts"
+    // );
+    // const pageEdges = allEdges.filter(
+    //     edge => edge.node.fields.collection === "pages"
+    // );
+
     result.data.allMarkdownRemark.edges.forEach(({ node }) => {
         createPage({
             path: node.frontmatter.path,
-            component: blogPostTemplate,
+            component: postTemplate,
             context: {}, // additional data can be passed via context
         });
     });
