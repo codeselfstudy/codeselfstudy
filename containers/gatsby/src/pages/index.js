6ae19e53d6867cf59043deb924a39ff58d0a14f0
J. Cohen <hostelmarketing@gmail.com>
2020-02-15 01:03:52 -0800

    Get homepage working. Cleanup.

--- containers/gatsby/src/pages/index.js
+++ containers/gatsby/src/pages/index.js
@@ -4,41 +4,19 @@ import { graphql } from "gatsby";
 import Layout from "../components/Layout";
 import SEO from "../components/SEO";
 
-// // The home page content is loaded from a TOML file (git submodule).
-// const tomlFile = path.resolve("../content/pages/index.toml");
-// console.log("========");
-// console.log("tomlFile", tomlFile);
-// const rawToml = fs.readFileSync(tomlFile);
-// console.log("rawToml", rawToml);
-// const pageData = toml.parse(rawToml);
-// console.log("pageData", pageData);
-// console.log("========");
+export default function IndexPage({ data }) {
+    const page = data.allIndexToml.edges.filter(
+        item => item.node.is_home === true
+    )[0].node;
 
-export default function IndexPage() {
-    // const { title, html_title, body } = data.allIndexToml.edges.node;
     return (
         <Layout>
-            <SEO isHome={true} title="home" />
-            <section className="section">
-                <h1 className="title is-1">ASDF</h1>
-                Dolor fugiat cumque amet eius distinctio enim? Quaerat provident
-                praesentium quo nam qui, dolorum. A dolores molestiae
-                praesentium veniam soluta Iste quia provident impedit quaerat
-                quae? Quia ratione nostrum asperiores
-            </section>
+            <SEO isHome={true} title={page.html_title} />
+
+            <div dangerouslySetInnerHTML={{ __html: page.body }}></div>
         </Layout>
     );
 }
-
-// return (
-//     <Layout>
-//         <SEO isHome={true} title={pageData.html_title} />
-//         <section className="section">
-//             <h1 className="title is-1">{pageData.title}</h1>
-//             {pageData.body}
-//         </section>
-//     </Layout>
-// );
 
 // `data.allIndexToml.edges.node`
 export const query = graphql`
@@ -46,6 +24,7 @@ export const query = graphql`
         allIndexToml {
             edges {
                 node {
+                    is_home
                     title
                     html_title
                     body
