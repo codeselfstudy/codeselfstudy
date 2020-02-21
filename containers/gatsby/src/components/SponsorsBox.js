50290b02c78f38b6e091e743cd67c6deb37dcd61
J. Cohen <hostelmarketing@gmail.com>
2020-02-20 19:29:56 -0800

    Add sponsor

--- containers/gatsby/src/components/SponsorsBox.js
+++ containers/gatsby/src/components/SponsorsBox.js
@@ -1,32 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "gatsby";
+import { shuffle } from "lodash";
 import digitalOceanLogo from "../images/sponsors/digital-ocean.png";
+import algoExpertLogo from "../content/files/algoexpert-logo.png";
 
-const sponsors = [
+const sponsorsData = [
     {
         id: 1,
         name: "Digital Ocean",
         src: digitalOceanLogo,
         href: "/sponsor/digital-ocean/",
     },
+    {
+        id: 2,
+        name: "AlgoExpert",
+        src: algoExpertLogo,
+        href: "/sponsor/algoexpert/",
+    },
 ];
+
+const sponsorStyles = {
+    margin: "17px",
+};
 
 function Sponsor({ name, src, href }) {
     return (
-        <Link to={href} title={name}>
-            <img src={src} alt={`${name} logo`} />
-        </Link>
+        <div className="sponsor" style={sponsorStyles}>
+            <Link to={href} title={name}>
+                <img src={src} alt={`${name} logo`} />
+            </Link>
+        </div>
     );
 }
 
 export default function SponsorsBox() {
+    const [sponsors, setSponsors] = useState([]);
+
+    // Randomize the sponsors in the box.
+    useEffect(() => {
+        setSponsors(shuffle(sponsorsData));
+    }, []);
+
     return (
         <div className="box">
             <h2 className="title is-2">Sponsors</h2>
             <p>
-                These companies provide assistance to our group. To support us
-                and add your logo, please <Link to="/contact/">contact us</Link>
-                .
+                These companies provide services, discounts, or other assistance
+                to our group. To support us and add your logo, please{" "}
+                <Link to="/contact/">contact us</Link>.
             </p>
             <div className="columns" style={{ margin: "27px 17px" }}>
                 {sponsors.map(s => (
