99b1846382c316c604fa396187fd07659f72b1ee
J. Cohen <hostelmarketing@gmail.com>
2020-02-22 15:40:36 -0800

    Fix graphql query

--- containers/gatsby/src/templates/PostTemplate.js
+++ containers/gatsby/src/templates/PostTemplate.js
@@ -59,6 +59,7 @@ export const pageQuery = graphql`
                 date(formatString: "MMMM DD, YYYY")
                 path
                 title
+                html_title
                 author
             }
         }
