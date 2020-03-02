4ee4d411e2e2d8e1592b7e300518e8d9bcb148bd
J. Cohen <hostelmarketing@gmail.com>
2020-03-02 15:35:49 -0800

    Tell Docker Compose where the docker-compose.production.yml file is

--- containers/nginx/init-letsencrypt.sh
+++ containers/nginx/init-letsencrypt.sh
@@ -30,7 +30,7 @@ fi
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
 mkdir -p "$data_path/conf/live/$domains"
-docker-compose run --rm --entrypoint "\
+docker-compose -f docker-compose.production.yml run --rm --entrypoint "\
     openssl req -x509 -nodes -newkey rsa:1024 -days 1\
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \
@@ -38,11 +38,11 @@ docker-compose run --rm --entrypoint "\
 echo
 
 echo "### Starting nginx ..."
-docker-compose up --force-recreate -d nginx
+docker-compose -f docker-compose.production.yml up --force-recreate -d nginx
 echo
 
 echo "### Deleting dummy certificate for $domains ..."
-docker-compose run --rm --entrypoint "\
+docker-compose -f docker-compose.production.yml run --rm --entrypoint "\
     rm -Rf /etc/letsencrypt/live/$domains && \
     rm -Rf /etc/letsencrypt/archive/$domains && \
     rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
@@ -64,7 +64,7 @@ esac
 # Enable staging mode if needed
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
-docker-compose run --rm --entrypoint "\
+docker-compose -f docker-compose.production.yml run --rm --entrypoint "\
     certbot certonly --webroot -w /var/www/certbot \
     $staging_arg \
     $email_arg \
@@ -75,4 +75,4 @@ docker-compose run --rm --entrypoint "\
 echo
 
 echo "### Reloading nginx ..."
-docker-compose exec nginx nginx -s reload
+docker-compose -f docker-compose.production.yml exec nginx nginx -s reload
