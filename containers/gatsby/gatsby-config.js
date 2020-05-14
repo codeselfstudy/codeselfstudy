4cda94b13d061533f7c6564b2224021ad41861cd
J. Cohen <hostelmarketing@gmail.com>
2020-05-14 03:58:12 -0700

    Fix CSS that is getting stripped out

--- containers/gatsby/gatsby-config.js
+++ containers/gatsby/gatsby-config.js
@@ -62,7 +62,15 @@ module.exports = {
             options: {
                 printRejected: true, // Print removed selectors and processed file names
                 develop: false, // Enable while using `gatsby develop`
-                // whitelist: ['whitelist'], // Don't remove this selector
+                whitelist: [
+                    "blockquote",
+                    "hero",
+                    "homepage-hero",
+                    "bg-circuit-board",
+                    "hero-body",
+                    "container",
+                    "content",
+                ],
                 // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
                 // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
             },
