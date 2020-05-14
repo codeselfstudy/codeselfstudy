eed4e308d9c289eec1346b43240f0b0b777a2d65
J. Cohen <hostelmarketing@gmail.com>
2020-05-14 03:06:37 -0700

    Add import to fix warning

--- containers/gatsby/src/pages/blog.js
+++ containers/gatsby/src/pages/blog.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "gatsby";
+import { Link, graphql } from "gatsby";
 
 import Layout from "../components/Layout";
 import SEO from "../components/SEO";
