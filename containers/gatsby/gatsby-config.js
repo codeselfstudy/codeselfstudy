93a3042a21a7348d80afc1d42915935c8cba9c59
J. Cohen <hostelmarketing@gmail.com>
2020-10-27 20:21:15 -0700

    Protect another CSS class

--- containers/gatsby/gatsby-config.js
+++ containers/gatsby/gatsby-config.js
@@ -78,6 +78,7 @@ module.exports = {
                     "button",
                     "is-large",
                     "is-link",
+                    "muted-link",
                 ],
                 // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
                 // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
