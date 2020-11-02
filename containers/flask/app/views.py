5dc8ab7a52d47dc9749d0ada711ff9f557f483fe
J. Cohen <hostelmarketing@gmail.com>
2020-11-01 19:11:42 -0800

    Queries work now

--- containers/flask/app/views.py
+++ containers/flask/app/views.py
@@ -1,10 +1,14 @@
 from pymongo import MongoClient
 from flask import render_template, jsonify, abort
+from flask_pymongo import PyMongo
 from app import app
 
-client = MongoClient("mongo", 27017)
-db = client["codeselfstudy"]
-collection = db["puzzles"]
+app.config["MONGO_URI"] = "mongodb://mongo/codeselfstudy"
+mongo = PyMongo(app)
+
+# client = MongoClient("mongo", 27017)
+# db = client["codeselfstudy"]
+# collection = db["puzzles"]
 # client = pymongo.MongoClient("mongodb://localhost:27017/")
 # r = collection.find_one()
 # print("#######", r)
@@ -35,11 +39,24 @@ def detail(source, puzzle_id):
     ```
     """
     if puzzle_id:
-        puzzle = collection.find_one({"source": source, "id": str(puzzle_id)})
+        puzzle = mongo.db.puzzles.find_one({"source": source, "id": str(puzzle_id)})
     else:
-        puzzle = collection.find_one({"source": source})
+        puzzle = mongo.db.puzzles.find_one({"source": source})
     if puzzle:
-        return jsonify(puzzle)
+        print("puzzle", puzzle)
+
+# {'_id': ObjectId('5f9f1ff4144695653459dbaf'), 'source': 'codewars', 'id': '50654ddff44f800200000004', 'name': 'Multiply', 'slug': 'multiply', 'category': 'bug_fixes', 'publishedAt': '2013-05-18T18:40:17.975Z', 'approvedAt': '2013-12-03T15:41:04.738Z', 'languages': ['javascript', 'coffeescript', 'ruby', 'python', 'haskell', 'clojure', 'java', 'csharp', 'elixir', 'cpp', 'typescript', 'php', 'crystal', 'dart', 'rust', 'fsharp', 'swift', 'go', 'shell', 'c', 'lua', 'sql', 'bf', 'r', 'nim', 'erlang', 'objc', 'scala', 'kotlin', 'solidity', 'groovy', 'fortran', 'nasm', 'julia', 'powershell', 'purescript', 'elm', 'ocaml', 'reason', 'idris', 'racket', 'agda', 'coq', 'vb', 'forth', 'factor', 'prolog', 'cfml', 'lean', 'cobol', 'haxe', 'commonlisp', 'raku', 'perl'], 'url': 'https://www.codewars.com/kata/50654ddff44f800200000004', 'rank': {'id': -8, 'name': '8 kyu', 'color': 'white'}, 'createdAt': '2012-09-28T07:12:31.171Z', 'approvedBy': {'username': 'alchemy', 'url': 'https://www.codewars.com/users/alchemy'}, 'description': 'This code does not execute properly. Try to figure out why.', 'totalAttempts': 3962887, 'totalCompleted': 3130189, 'totalStars': 1062, 'voteScore': 7822, 'tags': ['Bugs'], 'contributorsWanted': True, 'unresolved': {'issues': 4, 'suggestions': 1}}
+        return jsonify({
+            "id": puzzle["id"],
+            "url": puzzle["url"],
+            "name": puzzle["name"],
+            "languages": puzzle["languages"],
+            "kyu": abs(puzzle["rank"]["id"]),
+            "votes": puzzle["voteScore"],
+            "stars": puzzle["totalStars"],
+            "category": puzzle["category"],
+            "tags": puzzle["tags"],
+        })
     else:
         abort(404)
 
