04829fd472101a095e7e1e21582c3afaaf2e3ad5
J. Cohen <hostelmarketing@gmail.com>
2020-11-01 18:11:14 -0800

    Add flask routes

--- containers/flask/app/views.py
+++ containers/flask/app/views.py
@@ -1,7 +1,37 @@
-from flask import render_template
+from pymongo import MongoClient
+from flask import render_template, jsonify, abort
 from app import app
+
+client = MongoClient("mongo", 27017)
+db = client["codeselfstudy"]
+collection = db["puzzles"]
+# client = pymongo.MongoClient("mongodb://localhost:27017/")
 
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+
+@app.route("/<source>/<puzzle_id>")
+def detail(source, puzzle_id):
+    puzzle = collection.find_one({"source": source, "id": puzzle_id})
+    if puzzle:
+        return jsonify(puzzle)
+    else:
+        abort(404)
+
+
+@app.route("/search/codewars")
+def search_codewars():
+    pass
+
+
+@app.route("/search/projecteuler")
+def search_projecteuler():
+    pass
+
+
+@app.route("/search/leetcode")
+def search_leetcode():
+    pass
--- containers/flask/restart.sh
+++ containers/flask/restart.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-# Running this script will restart the app and load changes
+# Usage:
+# docker container exec -it codeselfstudy_flask_1 bash
+# ./restart.sh
+
+# Running this script will restart the Flask app and load changes
 touch uwsgi.ini
