34d5c0e1e4db3bfde1b43406a2ba32bb510711e4
J. Cohen <hostelmarketing@gmail.com>
2020-11-02 00:08:46 -0800

    Fix bug

--- containers/flask/app/helpers.py
+++ containers/flask/app/helpers.py
@@ -3,6 +3,9 @@
 
 def safe_list_get(lst, idx, default):
     """A helper function to safely get the first element of a list."""
+    if not lst:
+        return default
+
     try:
         return lst[idx]
     except IndexError:
