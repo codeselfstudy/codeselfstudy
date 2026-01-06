39564f722375b97955cbd4b47c7c21ec724bc250
Josh <backpack360@gmail.com>
2026-01-05 23:50:28 -0800

    Boilerplate sketches

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+export default function Navbar() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
