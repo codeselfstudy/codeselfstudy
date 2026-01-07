6b001b00e0eaab73e80edf6e9e6e176f5941e2c9
Josh <backpack360@gmail.com>
2026-01-07 00:29:23 -0800

    Switch to libsql for turso

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,11 @@
-import { drizzle } from "drizzle-orm/better-sqlite3";
+import { createClient } from "@libsql/client";
+import { drizzle } from "drizzle-orm/libsql";
 import * as schema from "./schema";
 import { env } from "@/env";
 
-export const db = drizzle(env.DATABASE_URL, { schema });
+const client = createClient({
+  url: env.DATABASE_URL,
+  authToken: env.TURSO_AUTH_TOKEN,
+});
+
+export const db = drizzle(client, { schema });
