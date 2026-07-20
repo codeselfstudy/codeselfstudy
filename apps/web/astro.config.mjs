e434a92110dae016fa5c1bc11b064729b914f547
Josh <backpack360@gmail.com>
2026-07-20 16:55:13 -0700

    Add Astro skeleton.

---
+++ apps/web/astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
+
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://codeselfstudy.com",
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
